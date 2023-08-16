8882adf66f51fdd27983cfcb867e20080b295644
Alex-gikungu <alexigikungu.012@gmail.com>
2023-08-16 12:25:00 +0300

    completed task

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    for count in range(10, 0,-1):
+        print(count)
+    print("Happy New Year!")
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    for sum in range(1,6,+1):
+        print(sum**2) 
+
+square_integers(sum)      
+
+
+def square_integers(int_list):
+    squared_list = []  
+    for num in int_list:
+        squared_list.append(num ** 2) 
+    return squared_list
+
+
+result = square_integers([1, 2, 3, 4, 5])
+print(result)  
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101): 
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
+
+
+
+
+
+
